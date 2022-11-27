a14370fcacf3d1fa3d2fc9ff14a1e8f510b84627
engrajibulhasan <engrajibulhasan@gmail.com>
2022-11-27 22:02:01 +0600

    Old repo need to upgrade

--- src/App.js
+++ src/App.js
@@ -1,67 +1,53 @@
-import './App.css';
 import React, { createContext, useState } from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
 
-import Home from './component/Home/Home';
-import Login from './component/Login/Login';
-import Header from './component/Header/Header';
-import Destination from './component/Destination/Destination';
-import PrivateRoute from './component/PrivateRoute/PrivateRoute';
+import Destination from "./component/Destination/Destination";
+import Header from "./component/Header/Header";
+import Home from "./component/Home/Home";
+import Login from "./component/Login/Login";
+import PrivateRoute from "./component/PrivateRoute/PrivateRoute";
 
 //Font Awesome Library for individual component use
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { fas } from '@fortawesome/free-solid-svg-icons'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
 
-
-export const UserContext=createContext();
+export const UserContext = createContext();
 
 //Passing fonts using Library
-library.add(fab,fas)
+library.add(fab, fas);
 function App() {
-  const [loggedInUser,setLoggedInUser]=useState({});
+  const [loggedInUser, setLoggedInUser] = useState({});
   return (
-    <UserContext.Provider value={[loggedInUser,setLoggedInUser]}>
-    <Router>
+    <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
+      <Router>
+        <>
+          <Header></Header>
+          <Switch>
+            <Route exact path="/">
+              <Home></Home>
+            </Route>
 
-      <>
-        
-        <Header></Header>
-        <Switch>
-          <Route exact path="/">
-            <Home></Home>
-          </Route>
+            <Route path="/home">
+              <Home></Home>
+            </Route>
 
-          <Route path="/home">
-            <Home></Home>
-          </Route>
+            <Route path="/login">
+              <Login></Login>
+            </Route>
 
-          <Route path="/login">
-            <Login></Login>
-          </Route>
-          
-          
+            <PrivateRoute exact path="/destination/:transportation">
+              <Destination></Destination>
+            </PrivateRoute>
 
-          <PrivateRoute exact path="/destination/:transportation">
-            <Destination></Destination>
-          </PrivateRoute>
-
-          <PrivateRoute path="/destination">
-            <Destination></Destination>
-          </PrivateRoute>
-
-          
-        </Switch>
-      </>
-
-    </Router>
+            <PrivateRoute path="/destination">
+              <Destination></Destination>
+            </PrivateRoute>
+          </Switch>
+        </>
+      </Router>
     </UserContext.Provider>
-    
   );
 }
 
